324c323e4fb5f18fcaf4a8cf273e95274948b682
Tarek ElBeih <ispoonjelly@Mr.kenedy>
2015-03-25 21:05:26 +0200

    added missing gender field to user and validated presence of required fields to user model #13

--- Looprac/db/schema.rb
+++ Looprac/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150325185137) do
+ActiveRecord::Schema.define(version: 20150325190312) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20150325185137) do
     t.string   "facebook"
     t.string   "twitter"
     t.string   "googleplus"
+    t.integer  "gender"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
