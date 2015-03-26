e6d47116cf9cfa2415905dc1c25577294b4a6cb5
Ahmed Abdel-Hamid <a_walid@live.com>
2015-03-26 19:17:13 +0200

    User types the username of the recipient instead of his email #1

--- Looprac/app/controllers/messages_controller.rb
+++ Looprac/app/controllers/messages_controller.rb
@@ -5,7 +5,7 @@ class MessagesController < ApplicationController
 	end
 
 	def create
-		recipients = User.where(email: params['recipients'])
+		recipients = User.where(username: params['recipients'])
 		conversation = current_user.send_message(recipients, params[:message][:body], params[:message][:subject]).conversation
 		redirect_to conversation_path(conversation)
 	end
