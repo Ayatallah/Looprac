34f9946211480855f830db68811098a0be384715
Tarek ElBeih <ispoonjelly@Mr.kenedy>
2015-04-03 15:17:07 +0200

    added banned field to user #19

--- Looprac/db/schema.rb
+++ Looprac/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150403131112) do
+ActiveRecord::Schema.define(version: 20150403131630) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 20150403131112) do
     t.string   "twitter"
     t.string   "googleplus"
     t.integer  "gender"
+    t.boolean  "banned"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
