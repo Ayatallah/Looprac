280a6bfdeb77f7546fd9f5c035e866a5fce2b8f8
ahmedtarek <ahmed.t.abdelfattah@gmail.com>
2015-04-04 15:03:49 +0200

    added all request info to view #2

--- Looprac/app/controllers/requests_controller.rb
+++ Looprac/app/controllers/requests_controller.rb
@@ -1,10 +1,16 @@
 class RequestsController < ApplicationController
 	def index
 		@requests=Request.where(:offerer_id => current_user.id)
-		@landmarks=Landmark.all
+
+		@landmarks=Array.new
 		@requesters=Array.new
 		@requests.each do |r|
-			@user=User.find(r.offerer_id)
+			@user=User.find(r.requester_id)
+			@ride=Ride.find(r.ride_id)
+			@source=Landmark.find(@ride.source_id)
+			@destination=Landmark.find(@ride.destination_id)
+			@landmarks.push(@source)
+			@landmarks.push(@destination)
 			@requesters.push(@user)
 		end	
 		#@requesters=User.where(:requester_id => )
--- Looprac/db/seeds.rb
+++ Looprac/db/seeds.rb
@@ -12,11 +12,16 @@
 #Todo.create!(title: 'google maps api', notes: 'you have to')
 #Todo.create!(title: 'JS!', notes: 'just do it')
 
-Ride.create(user_id: 2, source_id: 1, destination_id: 2, seatNum: 4, description: "Every day")
-Ride.create(user_id: 1, source_id: 3, destination_id: 1, seatNum: 2, description: "hmm")
+#Ride.create(user_id: 2, source_id: 1, destination_id: 2, seatNum: 4, description: "Every day")
+#Ride.create(user_id: 1, source_id: 3, destination_id: 1, seatNum: 2, description: "hmm")
 
-Ride.create(user_id: 3, source_id: 2, destination_id: 1, seatNum: 4, description: "Every day")
-Ride.create(user_id: 3, source_id: 3, destination_id: 2, seatNum: 2, description: "hmm")
+#Ride.create(user_id: 3, source_id: 2, destination_id: 1, seatNum: 4, description: "Every day")
+#Ride.create(user_id: 3, source_id: 3, destination_id: 2, seatNum: 2, description: "hmm")
 
-Request.create(offerer_id: 2, ride_id: 2, requester_id: 1)
+#Request.create(offerer_id: 2, ride_id: 2, requester_id: 1)
+Request.create(offerer_id: 2, ride_id: 13, requester_id: 3)
+
+#Landmark.create(name: "Helioplis", latitude: 1225, longitude: 2255, description: "chic")
+#Landmark.create(name: "Cairo Festival City", latitude: 1335, longitude: 22455, description: "shopping heaven")
+#Ride.create(user_id: 2, source_id: 4, destination_id: 5, seatNum: 2, description: "afafaaa")
 
