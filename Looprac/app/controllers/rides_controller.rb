5ae95404106d0475422e7ab099fc202ae4307ef6
Tarek ElBeih <ispoonjelly@Mr.kenedy>
2015-04-05 15:31:29 +0200

    added offering rides views with functioning controller methods #3

--- Looprac/app/controllers/rides_controller.rb
+++ Looprac/app/controllers/rides_controller.rb
@@ -1,12 +1,34 @@
 class RidesController < ApplicationController
 
 	def index
-	end
-
-	def show
-		@landmark = Landmark.all
+		@landmarks = Landmark.all
+		@ids = get_ids
 		@ride = Ride.new
-		render 'index'
 	end
 
+	def create
+  		@ride = Ride.new(ride_params)
+  		@ride.user_id = current_user.id
+		if @ride.save
+			flash[:notice] = 'Ride offered Successfuly!'
+    		redirect_to	'/rides'
+		else
+    		flash[:alert] = 'Could not offer this ride!'
+    		redirect_to	'/rides'
+  		end
+	end
+
+	def get_ids
+		landmarks = Landmark.all
+		landmark_id = []
+		landmarks.map do |landmark|
+				landmark_id << landmark.name
+				landmark_id << landmark.id
+		end
+		landmark_id
+	end
+
+	def ride_params
+		params.require(:ride).permit(:source_id, :destination_id, :seatNum, :description)
+	end
 end
