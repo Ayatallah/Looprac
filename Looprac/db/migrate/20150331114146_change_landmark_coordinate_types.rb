90ef9b8a5faf5f4a301c0c8878869ce8f5880407
M-Alfy <maealfy@gmail.com>
2015-04-07 02:04:51 +0200

    Possible improvements for the migrations files.

--- Looprac/db/migrate/20150331114146_change_landmark_coordinate_types.rb
+++
@@ -1,9 +0,0 @@
-class ChangeLandmarkCoordinateTypes < ActiveRecord::Migration
-  def change
-  	remove_column :landmarks, :longitude
-  	add_column :landmarks, :longitude, :float
-
-  	remove_column :landmarks, :latitude
-  	add_column :landmarks, :latitude, :float
-  end
-end
