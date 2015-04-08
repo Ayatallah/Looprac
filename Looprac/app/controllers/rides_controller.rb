e2c3d669cd43cc233e2e00974d64c27636c24240
ahmedtarek <ahmed.t.abdelfattah@gmail.com>
2015-04-08 15:09:46 +0200

    Handled same user request a ride more than once

--- Looprac/app/controllers/rides_controller.rb
+++ Looprac/app/controllers/rides_controller.rb
@@ -1,9 +1,17 @@
 class RidesController < ApplicationController
 	
 	def index
-		@rides=Ride.all.reverse
+		@rides=Ride.all
+		@rides_ids=Ride.pluck(:id)
 		@landmarks=Landmark.all 
 		@users=User.all
+		@ride_req_array=Array.new
+		@ride_req_array.push([])
+		@rides_ids.each do |r|
+			@temp=Request.where(:ride_id => r).pluck(:requester_id)
+			@ride_req_array.push(@temp)
+		end	
+		@rides=@rides.reverse
 	end
 
 	def show
