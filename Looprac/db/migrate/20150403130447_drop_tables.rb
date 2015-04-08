1b3a587c7fad64c4184684df6e1e44c56434d95d
M-Alfy <maealfy@gmail.com>
2015-04-09 00:25:56 +0300

    Revert "Add new landmark"

--- Looprac/db/migrate/20150403130447_drop_tables.rb
+++
@@ -1,6 +0,0 @@
-class DropTables < ActiveRecord::Migration
-  def change
-  	drop_table :rides
-  	drop_table :requests
-  end
-end
--- Looprac/test/controllers/landmark_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class LandmarkControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
