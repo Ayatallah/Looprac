f46af57461017a593b4f39f1dce4d5f9be6b256b
M-Alfy <maealfy@gmail.com>
2015-04-08 16:08:40 +0200

    Updated Models' syntax and now search leads to all rides. #5

--- Looprac/app/models/landmark.rb
+++ Looprac/app/models/landmark.rb
@@ -2,6 +2,6 @@ class Landmark < ActiveRecord::Base
 	validates :name, presence: true
 	validates :longitude, presence: true
 	validates :latitude, presence: true
-	has_many :source, :class_name => 'Ride', :foreign_key => 'source_id'
-	has_many :destination, :class_name => 'Ride', :foreign_key => 'destination_id'
+	has_many :source, class_name:'Ride', foreign_key: 'source_id'
+	has_many :destination, class_name: 'Ride', foreign_key: 'destination_id'
 end
--- Looprac/app/models/ride.rb
+++ Looprac/app/models/ride.rb
@@ -3,8 +3,8 @@ class Ride < ActiveRecord::Base
 	validates :source_id, presence: true
 	validates :destination_id, presence: true
 	validates :seatNum, presence: true
-	belongs_to :source, :class_name => 'Landmark'
-	belongs_to :destination, :class_name => 'Landmark'
+	belongs_to :source, class_name: 'Landmark'
+	belongs_to :destination, class_name: 'Landmark'
 	belongs_to :user, :class_name => 'User'
 	has_many :ride_in_request, :class_name => 'Request', :foreign_key => 'ride_id'
 end
