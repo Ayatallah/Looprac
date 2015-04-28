ddeb4a2b76c7d966155a3b2f9a19f108bd7ca93d
Ahmed Abdel-Hamid <a_walid@live.com>
2015-04-29 01:52:25 +0300

    Notifies user in the navbar when a request is sent to him #74

--- Looprac/app/helpers/requests_helper.rb
+++ Looprac/app/helpers/requests_helper.rb
@@ -1,5 +1,6 @@
 module RequestsHelper
 	# Calculates the number of requests that the user hasn't responded to yet
+	# @author:Zuishek
 	def pending_ride_requests_helper(user)
     Request.where(:offerer_id => user.id).where(:response => nil).count
     end
