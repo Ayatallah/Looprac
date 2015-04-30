0c3be9dd631f7c977b14db232e91fd9b970aef8c
Your Name <ayatallah.amr@gmail.com>
2015-04-30 23:53:23 +0300

    Added Rating Modifications Documentation #72

--- Looprac/app/controllers/users_controller.rb
+++ Looprac/app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 		@useratings= Userating.where(rated_id: @user.id)
 		@userating= Userating.where(rated_id: @user.id, rater_id: current_user.id)
+		# @author : Ayatallah
+		# @requests is the Accepted Ride requests between User viewed and Current user
 		@requests = Request.where(offerer_id: @user.id, requester_id: current_user.id, response: true)
 	end
 
