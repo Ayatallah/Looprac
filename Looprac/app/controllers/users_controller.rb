0753e53fa15ed03acfbae743c4f9da24b5e94bc8
ISpoonJelly <tiitomoha@gmail.com>
2015-04-30 17:40:11 +0200

    made the report link only visible to people who taken a ride with a given user #87

--- Looprac/app/controllers/users_controller.rb
+++ Looprac/app/controllers/users_controller.rb
@@ -3,9 +3,17 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
-		@reports = Report.where("reporter" == "ISpoonJelly").pluck("reported")
-		@useratings= Userating.where(rated_id: @user.id)
-		@userating= Userating.where(rated_id: @user.id, rater_id: current_user.id)
+		@reports = Report.where("reporter" == User.find_by_id(@user.id).username).pluck("reported")
+		@tookRideWith = Request.where("requester_id" => @user.id).where("response" => true).pluck("offerer_id")
+		@gaveRideTo = Request.where("offerer_id" => @user.id).where("response" => true).pluck("requester_id")
+		puts @user.id
+		puts "HEY1!"
+		puts @tookRideWith
+		puts "HEY2!"
+		puts @gaveRideTo
+
+		@useratings = Userating.where(rated_id: @user.id)
+		@userating = Userating.where(rated_id: @user.id, rater_id: current_user.id)
 	end
 
 	def index
