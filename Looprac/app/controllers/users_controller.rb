e61628b44e92d90c25b3f6466efc0664029beeb8
ISpoonJelly <tiitomoha@gmail.com>
2015-04-30 13:28:06 +0200

    added the report link to the user profile and made the link hidden if the logged in user previously reported the viewed profile #87

--- Looprac/app/controllers/users_controller.rb
+++ Looprac/app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
+		@reports = Report.where("reporter" == "ISpoonJelly").pluck("reported")
 		@useratings= Userating.where(rated_id: @user.id)
 		@userating= Userating.where(rated_id: @user.id, rater_id: current_user.id)
 	end
