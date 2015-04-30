f77344c5a651ddb53a829a5935a2b6d9448b5c31
Your Name <ayatallah.amr@gmail.com>
2015-04-30 22:10:22 +0300

    Increase points with profile completion #71

--- Looprac/app/helpers/users_helper.rb
+++ Looprac/app/helpers/users_helper.rb
@@ -3,18 +3,23 @@ module UsersHelper
 	    user.percentage = 50
 	    if user.facebook != ""
 	      user.percentage += 10
+	      user.points += 50
 	    end
 	    if user.twitter != ""
 	      user.percentage += 10
+	      user.points += 50
 	    end
 	    if user.googleplus != ""
 	      user.percentage += 10
+	      user.points += 50
 	    end
 	    if user.age > 0
 	      user.percentage += 10
+	      user.points += 50
 	    end
 	    if user.car_model != ""
 	      user.percentage += 10
+	      user.points += 50
     end
 end
 end
