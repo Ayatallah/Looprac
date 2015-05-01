eefe5fe74dea611ba1e2d8239410bca2a95401ee
ISpoonJelly <tiitomoha@gmail.com>
2015-05-01 14:11:51 +0200

    added distance calculation and pricing to Rides #85

--- Looprac/app/controllers/rides_controller.rb
+++ Looprac/app/controllers/rides_controller.rb
@@ -32,6 +32,10 @@ class RidesController < ApplicationController
 	end
 
 	def create
+		@from = Landmark.find_by_id(ride_params[:source_id])
+		@to = Landmark.find_by_id(ride_params[:destination_id])
+		@distance = distance [@from.latitude, @from.longitude], [@to.latitude, @to.longitude]
+		@distance = @distance * 5 + current_user.rank
   		@ride = Ride.new(ride_params)
   		@ride.user_id = current_user.id
 		if @ride.save
@@ -69,8 +73,22 @@ class RidesController < ApplicationController
 		landmark_id
 	end
 
+	def distance loc1, loc2
+	  rad_per_deg = Math::PI/180  # PI / 180
+	  rkm = 6371                  # Earth radius in kilometers
+
+	  dlat_rad = (loc2[0]-loc1[0]) * rad_per_deg  # Delta, converted to rad
+	  dlon_rad = (loc2[1]-loc1[1]) * rad_per_deg
+
+
+	  a = Math.sin(dlat_rad/2)**2 + Math.cos(loc1[0] * rad_per_deg) * Math.cos(loc2[0] * rad_per_deg) * Math.sin(dlon_rad/2)**2
+	  c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
+
+	  rkm * c # Delta in Kilometers
+	end
+
 	def ride_params
-		params.require(:ride).permit(:source_id, :destination_id, :seatNum, :datetime, :description, :ac, :music, :smoking, :food, :pets)
+		params.require(:ride).permit(:source_id, :destination_id, :seatNum, :datetime, :description, :ac, :music, :smoking, :food, :pets).merge(price: @distance)
 	end
 
 
