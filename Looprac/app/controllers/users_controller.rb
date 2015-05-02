5d8d14ff93ad1943df7014512c7bdd5551d16c96
Your Name <ayatallah.amr@gmail.com>
2015-05-02 22:30:32 +0300

    fix user history conflict

--- Looprac/app/controllers/users_controller.rb
+++ Looprac/app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 		@gaveRideTo = Request.where("offerer_id" => @user.id).where("response" => true).pluck("requester_id")
 		@useratings = Userating.where(rated_id: @user.id)
 		@userating = Userating.where(rated_id: @user.id, rater_id: current_user.id)
+		@requests = Request.where(offerer_id: @user.id, requester_id: current_user.id, response: true)
 	end
 
 	def index
@@ -26,6 +27,17 @@ class UsersController < ApplicationController
 	def ratings
 		@user = User.find(params[:id])
 		@useratings= Userating.where(rated_id: @user.id)
+	end
+
+	def reports
+		@user = User.find(params[:id])
+		@reports = Report.where("reported" == User.find_by_id(@user.id).username)
+	end
+	
+	def rides
+		@user = User.find(params[:id])
+		@landmarks = Landmark.all
+		@rides = Ride.where(user_id: @user.id)
 	end
 
 
