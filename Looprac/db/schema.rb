39d1aff4c6b783d194e473fe0c1e4bab59a47b76
Karim Taha <karimtaha7000@gmail.com>
2015-05-02 20:57:18 +0200

    Fixed index page, changed containers in profiles to round #118

--- Looprac/db/schema.rb
+++ Looprac/db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150501144921) do
+ActiveRecord::Schema.define(version: 20150501184642) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
